28bdfb43c1db9121610abf8d5dd4e94766e8e591
Joshua Mark <joshuajmark@gmail.com>
2016-04-15 17:12:09 -0700

    Cleanup and dev URL fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,21 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
 require 'jwt'
-# before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
   # POST /users/sign_in
   def create
     super
   end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
